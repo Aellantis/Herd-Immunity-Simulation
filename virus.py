0315de6792a687f9a70f06da061ae0487f162e82
Aellantis <Ashleyyung.1231@gmail.com>
2024-11-27 21:37:59 -0800

    added tests for virus.py

--- virus.py
+++ virus.py
@@ -6,6 +6,8 @@ class Virus(object):
         # TODO Define the other attributes of Virus
         self.repro_rate = repro_rate
         self.mortality_rate = mortality_rate
+    def __repr__(self):
+        return f"Virus(name={self.name}, repro_rate={self.repro_rate}, mortality_rate={self.mortality_rate})"
 
 
 # Test this class
@@ -19,3 +21,15 @@ if __name__ == "__main__":
     print(virus.name)
     print(virus.repro_rate)
     print(virus.mortality_rate)
+
+    another_virus = Virus("Flu", 0.5, 0.1)
+    assert another_virus.name == "Flu"
+    assert another_virus.repro_rate == 0.5
+    assert another_virus.mortality_rate == 0.1
+    print(another_virus.name)
+    print(another_virus.repro_rate)
+    print(another_virus.mortality_rate)
+    print("Second virus test passed!")
+
+    assert repr(virus) == "Virus(name=HIV, repro_rate=0.8, mortality_rate=0.3)"
+    print("Virus repr test passed!")
